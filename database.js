c44df790ae779c7c726f5cac3447d2a859a259a3
KSPRajah <136917659+KSPRajah@users.noreply.github.com>
2023-06-19 16:15:40 +0530

    databaset.js

--- database.js
+++ database.js
@@ -36,17 +36,23 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
 
         db.run(`CREATE TABLE suppliers (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            supplierName text, 
+            name text, 
             address text,
-            joinedDate text,
-            mobileNo text
+            emai text,
+            gender text,
+            age INTEGER,
+            cardHolderName text,
+            cardNumber INTEGER,
+            expiryDate text,
+            cvv INTEGER,
+            timeStamp text
             )`, (err) => {
             if (err) {
                 // Table already created
             } else {
                 // Table just created, creating some rows
-                var insert = 'INSERT INTO suppliers (supplierName, address, joinedDate, mobileNo) VALUES (?,?,?,?)'
-                db.run(insert, ["D.J.Ishara", "345A ,R.A De Mel Road, Colombo 3", "16/3/2022", "0776600933"])
+                var insert = 'INSERT INTO suppliers (name, address, emai, gender,age,cardHolderName,cardNumber,expiryDate,cvv,timeStamp) VALUES (?,?,?,?,?,?,?,?,?,?)'
+                db.run(insert, [”A.D.Lakith Dharmasiri”,”No 324/A Ra De Mel Road, Colombo”,”lakith@gmail.com”,”female”,28,”A.D.L.Dharmasiri”,102445217895,”12/2022”,246,”2022-12-31 23:59:59”])
 
             }
         })
